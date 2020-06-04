64ef76852e3d40534087eda54c21dff97ba3a2f2
wf4118 <66257538+wf4118@users.noreply.github.com>
2020-06-04 14:25:28 +0800

    Add drop down menu

--- lib/find_screen.dart
+++ lib/find_screen.dart
@@ -26,71 +26,13 @@ Column(
       height: 200,
       child: label(),
     ),
+    Container(
+      height: 300,
+      child: dropdownScreen(),
+    ),
   ],
 );
 // #enddocregion Column
-
-class label extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => new labelState();
-}
-
-class labelState extends State<label> {
-  final List<Tab> tabs = <Tab>[
-    new Tab(
-      text: 'composer',
-    ),
-    new Tab(
-      text: 'arranger',
-    ),
-    new Tab(
-      text: 'singer',
-    ),
-    new Tab(
-      text: 'lyrics writer',
-    ),
-    new Tab(
-      text: 'MV maker',
-    ),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return new DefaultTabController(
-      length: tabs.length,
-      child: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Find partner'),
-          bottom: new TabBar(
-            tabs: tabs,
-            isScrollable: true,
-            labelColor: Colors.blueGrey,
-            unselectedLabelColor: Colors.white,
-            labelStyle: new TextStyle(fontSize: 18.0),
-          ),
-        ),
-        body: new TabBarView(
-          children: tabs.map((Tab tab) {
-            return new Center(
-              child: buildAvatarRow(),
-            );
-          }).toList(),
-        ),
-      ),
-    );
-  }
-}
-
-Widget buildAvatarRow() =>
-    // #docregion Row
-Row(
-  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  children: [
-    Image.asset('images/photo.jpg'),
-    Image.asset('images/photo.jpg'),
-    Image.asset('images/photo.jpg'),
-  ],
-);
-// #enddocregion Row
 
 class SearchBar extends StatefulWidget {
   @override
@@ -103,6 +45,7 @@ class _SearchBarState extends State<SearchBar> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar:AppBar(
+            title: Text('look up'),
             actions:<Widget>[
               IconButton(
                   icon:Icon(Icons.search),
@@ -184,3 +127,168 @@ class searchBarDelegate extends SearchDelegate<String>{
     );
   }
 }
+
+class label extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new labelState();
+}
+
+class labelState extends State<label> {
+  final List<Tab> tabs = <Tab>[
+    new Tab(
+      text: 'composer',
+    ),
+    new Tab(
+      text: 'arranger',
+    ),
+    new Tab(
+      text: 'singer',
+    ),
+    new Tab(
+      text: 'lyrics writer',
+    ),
+    new Tab(
+      text: 'MV maker',
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return new DefaultTabController(
+      length: tabs.length,
+      child: new Scaffold(
+        appBar: TabBar(
+          tabs: tabs,
+          isScrollable: true,
+          labelColor: Colors.black87,
+          unselectedLabelColor: Colors.blueGrey,
+          labelStyle: new TextStyle(fontSize: 18.0),
+        ),
+        body: new TabBarView(
+          children: tabs.map((Tab tab) {
+            return new Center(
+              child: buildAvatarRow(),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildAvatarRow() =>
+    // #docregion Row
+Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+    buildAvatarNamePair(),
+    buildAvatarNamePair(),
+    buildAvatarNamePair(),
+  ],
+);
+// #enddocregion Row
+
+Widget buildAvatarNamePair() =>
+    // #docregion Row
+Column(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+    Image.asset('images/photo.jpg'),
+    Text('Someone'),
+  ],
+);
+// #enddocregion Row
+
+class Item {
+  const Item(this.name,this.icon);
+  final String name;
+  final Icon icon;
+}
+
+class dropdownScreen extends StatefulWidget {
+  State createState() =>  dropdownScreenState();
+}
+
+class dropdownScreenState extends State<dropdownScreen> {
+  Item selectedGenre;
+  List<Item> genres = <Item>[
+    const Item('All',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('Blues',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('R&B',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('Jazz',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('Rock',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('Country',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('Soul',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('Dance',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('Hip-hop',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('C-pop',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+    const Item('Other',Icon(Icons.arrow_forward_ios,color:  Colors.blueGrey,)),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: DropdownButton<Item>(
+          hint:  Text("Select genres"),
+          value: selectedGenre,
+          onChanged: (Item Value) {
+            setState(() {
+              selectedGenre = Value;
+            });
+          },
+          items: genres.map((Item genre) {
+            return  DropdownMenuItem<Item>(
+              value: genre,
+              child: Row(
+                children: <Widget>[
+                  genre.icon,
+                  SizedBox(width: 10,),
+                  Text(
+                    genre.name,
+                    style:  TextStyle(color: Colors.black87),
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+      body: buildCoorperationList(),
+    );
+  }
+}
+
+Widget buildCoorperationList() =>
+    Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buildCoorperation(),
+        buildCoorperation(),
+        buildCoorperation(),
+      ],
+    );
+
+Widget buildCoorperation() =>
+    // #docregion Row
+Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+    Image.asset('images/music.jpg'),
+    buildNameNeedPair(),
+    IconButton (
+      icon: Icon(Icons.person_add, color: Colors.blueGrey,),
+    ),
+    IconButton (
+      icon: Icon(Icons.more_horiz, color: Colors.blueGrey,),
+    ),
+  ],
+);
+// #enddocregion Row
+
+Widget buildNameNeedPair() =>
+    Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text('Someone'),
+        Text('looking for a composer')
+      ],
+    );
