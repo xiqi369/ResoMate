302971c1452353270e87d7769a0959aab86f60a0
xiqi369 <47092868+xiqi369@users.noreply.github.com>
2020-06-04 22:17:31 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,168 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'findPage.dart';
+import 'emptyPage.dart';
 
-import 'detail_page.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ResoMate',
-      debugShowCheckedModeBanner: false,
-      theme:
-          ThemeData(primarySwatch: Colors.blue, accentColor: Color(0xff0DBC83)),
-      home: HomePage(),
-    );
-  }
+void main(){
+  runApp(
+      new MaterialApp(
+          title:'ResoMate',
+          home:new MyApp()
+      )
+  );
 }
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() =>new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin{
+
+  TabController tabcontroller;
+
+  @override
+  void initState() {
+    super.initState();
+    tabcontroller = new TabController(
+        length: 5,
+        vsync: this
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        elevation: 0,
-        title: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'TRENDING',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
-            ],
-          ),
+    return new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Color(0xFF2F4F4F),
         ),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Container(
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    image: AssetImage(
-                      'assets/face.jpg',
-                    ),
-                    fit: BoxFit.cover),
-              ),
-            ),
-          ),
-        ],
-      ),
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: new TabBarView(
+          controller: tabcontroller,
           children: <Widget>[
-            SizedBox(
-              width: double.infinity,
-              height: 200,
-              child: Swiper(
-                itemCount: 5,
-                autoplay: false,
-                viewportFraction: 0.7,
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DetailPage(
-                                    heroTag: index.toString(),
-                                  )));
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(16.0),
-                        child: Stack(
-                          children: <Widget>[
-                            Positioned.fill(
-                                child: Hero(
-                              tag: '$index',
-                              child: Image.asset('assets/$index.png',
-                                  fit: BoxFit.cover),
-                            )),
-                            Positioned(
-                              top: 8,
-                              right: 8,
-                              child: Icon(
-                                Icons.favorite_border,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Positioned(
-                              bottom: 0,
-                              left: 0,
-                              right: 0,
-                              child: Container(
-                                color: Colors.black12,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    'Flower dance',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontFamily: 'Sans',
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Golden Musicians',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: 'Sans',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18),
-              ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 150,
-              child: ListView.builder(
-                itemCount: 5,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.asset(
-                        'assets/$index.png',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            )
+            new emptyPage(),
+            new Find(),
+            new emptyPage(),
+            new emptyPage(),
+            new emptyPage(),
           ],
         ),
-      ),
+        bottomNavigationBar: new Material(
+          color: Color(0xFF8FBC8F),
+          child: new TabBar(
+            controller: tabcontroller,
+            tabs: <Tab>[
+              new Tab(text: "Home",icon: new Icon(Icons.home)),
+              new Tab(text: "Find",icon: new Icon(Icons.search)),
+              new Tab(text: "Upload",icon: new Icon(Icons.file_upload)),
+              new Tab(text: "Chat",icon: new Icon(Icons.chat)),
+              new Tab(text: "Me",icon: new Icon(Icons.person)),
+            ],
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.black,
+          ),
+        )
     );
+  }
+
+  @override
+  void dispose() {
+    tabcontroller.dispose();
+    super.dispose();
   }
 }
